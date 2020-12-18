a86342da648d695bbd9db43365b1ae65094fb353
Tim Juretzek <tj@jur-solutions.de>
2020-12-18 16:19:53 +0100

    bugfixing on the theme layer

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -53,7 +53,7 @@ const ThemeContainer = styled.div`
 
 const MenuFlyout = styled.span`
   max-width: 100vw;
-  min-width: 11rem;
+  min-width: 11.5rem;
   background-color: ${({ theme }) => theme.modalBG};
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
