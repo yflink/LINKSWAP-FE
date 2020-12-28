42e3b84bec3a9bc6b04b73a6c25bb51a4e0ca427
Tim Juretzek <tj@jur-solutions.de>
2020-12-28 20:08:37 +0100

    style fixes
    # Conflicts:
    #	src/pages/AppBody.tsx

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -13,12 +13,12 @@ export const BodyWrapper = styled.div`
 `
 
 export const BodyWrapperDark = styled.div`
-  margin-top: -3px;
+  margin: 0 10px;
   padding: 1.5rem;
   padding-top: calc(1.5rem + 3px);
   position: relative;
   width: 100%;
-  max-width: 420px;
+  max-width: 400px;
   background: ${({ theme }) => theme.modalFooterBG};
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
