9e0c4c1b20a466f0ed87d4d8b4feae267bcf6c87
Tim Juretzek <tj@jur-solutions.de>
2020-12-30 16:53:44 +0100

    animation fix for mobile

--- src/theme/settings/doki.ts
+++ src/theme/settings/doki.ts
@@ -83,10 +83,10 @@ export function dokiDokiTheme() {
     //Global
     bodyBG: '#c0aff5 url("../images/themes/dokidoki/background_large.png") center center / cover no-repeat',
     bodyBGTablet: '#c0aff5 url("../images/themes/dokidoki/background_tablet.png") center center / cover no-repeat',
-    bodyBGMobile: '#c0aff5',
+    bodyBGMobile: '#c0aff5 url("../images/themes/dokidoki/background_mobile.png") center center / cover no-repeat',
     layerBG: '#c0aff5 url("../images/themes/dokidoki/background_large.gif") center center / cover no-repeat',
     layerBGTablet: '#c0aff5 url("../images/themes/dokidoki/background_large.gif") center center / cover no-repeat',
-    layerBGMobile: '#c0aff5 url("../images/themes/dokidoki/background_large.gif") center center / cover no-repeat',
+    layerBGMobile: transparent,
     bodyBGColor: bodyBGColor,
     linkColor: mainColor,
     linkColorHover: mainColor,
