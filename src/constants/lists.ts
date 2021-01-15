8021ef5bb3e15707fcc0115c4bbdfe1575bfaca0
Tim Juretzek <tj@jur-solutions.de>
2021-01-15 12:45:58 +0100

    fixed all token function

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
-export const DEFAULT_TOKEN_LIST_URL = 'https://staging.linkswap.app/lists/linkswapTokenList.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://linkswap.app/lists/linkswapTokenList.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'https://staging.linkswap.app/lists/linkcheckVerified.json'
+  'https://linkswap.app/lists/linkcheckVerified.json'
 ]
